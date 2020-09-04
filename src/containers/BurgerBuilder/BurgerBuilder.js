39dd481e98f1eab1a040eb038dfd8c047684bf9b
surelay <hooddevelopers@gmail.com>
2020-09-04 01:13:36 +0100

    added routing and a contact component to the app

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,29 +93,38 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-
-        this.setState( { loading: true } );
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Max Schwarzmüller',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '41351',
-                    country: 'Germany'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-         axios.post( '/orders.json', order )
-            .then( response => {
-                this.setState( { loading: false, purchasing: false } );
-            } )
-            .catch( error => {
-                this.setState( { loading: false, purchasing: false } );
-            } );
+        //{/*
+        //this.setState( { loading: true } );
+       // const order = {
+        //    ingredients: this.state.ingredients,
+          //  price: this.state.totalPrice,
+          //  customer: {
+             //   name: 'Max Schwarzmüller',
+             //   address: {
+                 //   street: 'Teststreet 1',
+                //    zipCode: '41351',
+             //       country: 'Germany'
+             //   },
+            //    email: 'test@test.com'
+          //  },
+         //   deliveryMethod: 'fastest'
+     //   }
+     //    axios.post( '/orders.json', order )
+      //      .then( response => {
+        //        this.setState( { loading: false, purchasing: false } );
+       //     } )
+       //     .catch( error => {
+       //         this.setState( { loading: false, purchasing: false } );
+          //  } );*/}
+            const queryParams = [];
+            for (let i in this.state.ingredients) {
+                queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+            }
+            const queryString = queryParams.join('&');
+            this.props.history.push({
+                pathname : '/checkout',
+                search : '?' + queryString
+            });
     }
 
     render () {
